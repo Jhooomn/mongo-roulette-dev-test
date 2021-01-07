a0c11c4a68f3e717b7987185eaad8a62412f2cb1
Jhon Baron <jhon.baron@zabud.com.co>
2021-01-07 08:53:10 -0500

    updated:
    - path refactor was necessary because of project name

--- src/main/java/com/masiv/test/JhonBaronDevTestApplication.java
+++ src/main/java/com/masiv/test/JhonBaronDevTestApplication.java
@@ -1,4 +1,4 @@
-package com.masiv.test.JhonBaronDevTest;
+package com.masiv.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
