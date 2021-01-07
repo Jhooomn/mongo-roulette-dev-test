80ed9f2e352d7eb9b8be6d98d9b985e6c4373faf
Jhon Baron <jhon.baron@zabud.com.co>
2021-01-07 11:48:34 -0500

    added:
    - - - > create bet color type to validate which colors can be accepted in the app-db

--- src/main/java/com/masiv/test/domain/enums/betColor/BetColorTypeConverter.java
+++ src/main/java/com/masiv/test/domain/enums/betColor/BetColorTypeConverter.java
@@ -1,8 +1,20 @@
 package com.masiv.test.domain.enums.betColor;
 
+import java.util.Objects;
+import javax.persistence.AttributeConverter;
 import javax.persistence.Converter;
 
 @Converter
-public class BetColorTypeConverter {
+public class BetColorTypeConverter implements AttributeConverter<BetColorType, String> {
 
+  @Override
+  public String convertToDatabaseColumn(BetColorType betColorType) {
+    if (!Objects.isNull(betColorType)) return betColorType.getId();
+    return BetColorType.NOT_APPLY.getId();
+  }
+
+  @Override
+  public BetColorType convertToEntityAttribute(String s) {
+    return BetColorType.findByPrimaryKey(s);
+  }
 }
