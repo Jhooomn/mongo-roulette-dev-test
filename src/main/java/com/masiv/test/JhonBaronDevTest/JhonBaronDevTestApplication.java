d96b34a95e00f974de8ac7681cad13f2b32e742f
Jhon Baron <jhon.baron@zabud.com.co>
2021-01-07 08:30:15 -0500

    first commit

---
+++ src/main/java/com/masiv/test/JhonBaronDevTest/JhonBaronDevTestApplication.java
@@ -0,0 +1,13 @@
+package com.masiv.test.JhonBaronDevTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhonBaronDevTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhonBaronDevTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/masiv/test/JhonBaronDevTest/JhonBaronDevTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.masiv.test.JhonBaronDevTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JhonBaronDevTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
